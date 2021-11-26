9952f648c85787ca19b6e77d4c13a531aa0e2041
TracyWang16 <tracywangee16@gmail.com>
2021-11-26 15:16:58 +0800

    add controled prompt generation

---
+++ openprompt/data_utils/control_generation_dataset.py
@@ -0,0 +1,98 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This file contains the logic for loading data for all Conditional Generation tasks.
+"""
+
+from openprompt.data_utils.utils import InputExample
+import os
+import json, csv
+from abc import ABC, abstractmethod
+from collections import defaultdict, Counter
+from typing import List, Dict, Callable
+
+from openprompt.utils.logging import logger
+from openprompt.data_utils.data_processor import DataProcessor
+
+
+def select_examples(example_file, example_num):
+    fin_sele =  open (example_file,'r')
+    text_dict = json.loads(fin_sele.read())
+    text_list = [text['comment_text'].replace('\\n','') for text in text_dict]
+    text_list = [text.replace('\n','') for text in text_list]
+    while len(text_list)<example_num:
+        text_list = text_list+text_list
+    #return sample(text_list,example_num)
+    return text_list[0:example_num]
+    
+'''
+def add_examples(prompts, pos_file=None, neg_file=None):
+    if pos_file is not None and neg_file is not None:
+        pos_examples = select_examples(pos_file,len(prompts))
+        neg_examples = select_examples(neg_file,len(prompts))
+        control_template_list = [ control_template.replace('POS',pos_examples[i]) for i in range(len(pos_examples)) ]
+        control_template_list = [ control_template_list[i].replace('NEG',neg_examples[i]) for i in range(len(control_template_list)) ]
+    elif pos_file is not None:
+        pos_examples = select_examples(pos_file,len(prompts))
+        control_template_list = [ control_template.replace('POS',pos_examples[i]) for i in range(len(pos_examples)) ]
+    elif neg_file is not None:
+        neg_examples = select_examples(neg_file,len(prompts))
+        control_template_list = [ control_template.replace('NEG',neg_examples[i]) for i in range(len(neg_examples)) ]
+    prompts = [control_template_list[i].replace('INP',prompts[i]) for i in range(len(prompts))]
+    return prompts
+'''
+
+class ToxicyProcessor(DartProcessor):
+    def __init__(self):
+        super().__init__()
+        self.labels = None
+
+    def get_examples(self, data_path:str, line_by_line=False, add_neg_example=False, add_pos_example=False, pos_example_filepath=None, neg_example_filepath=None):
+        examples = []
+        assert data_path.endswith('.jsonl') 
+        dataset = pd.read_json(data_path, lines=True)         #prompts/nontoxic_prompts-10k.jsonl
+        prompts = pd.json_normalize(dataset['prompt'])['text']
+
+        if add_pos_example:
+            pos_examples = select_examples(example_file=pos_example_filepath, example_num=len(prompts))
+        if add_neg_example:
+            neg_examples = select_examples(example_file=neg_example_filepath, example_num=len(prompts))
+
+        if add_pos_example and add_neg_example:
+            for i, (text, pos_example, neg_example) in enumerate(zip(prompts, pos_examples, neg_examples)):
+                example = InputExample(guid=str(i),text_a=text, tgt_text=text, meta={'pos':pos_example, 'neg':neg_example})
+                examples.append(example)
+
+        elif add_pos_example:
+            for i, (text, pos_example) in enumerate(zip(prompts,pos_examples):
+                example = InputExample(guid=str(i),text_a=text, tgt_text=text, meta={'pos':pos_example})
+                examples.append(example)
+
+        elif add_neg_example:
+            for i, (text,neg_example) in enumerate(zip(prompts,neg_examples):
+                example = InputExample(guid=str(i),text_a=text, tgt_text=text, meta={'neg':neg_example})
+                examples.append(example)
+
+        else:
+            for i,text in enumerate(prompts):
+                example = InputExample(guid=str(i),text_a=text, tgt_text=text)
+                examples.append(example)
+        
+        return examples
+    
+
+PROCESSORS = {
+    "toxicity": ToxicyProcessor,
+    # "e2e": E2eProcessor,
+    # "dart" : DartProcessor,
+}
